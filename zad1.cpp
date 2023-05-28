dbc0443a579040f23488402f0d3e34d7099a2e7c
Filip Wojnicki <filip.wojnicki@op.pl>
2023-05-28 09:28:30 +0200

    zad 1

--- zad1.cpp
+++ zad1.cpp
@@ -1,1 +1,23 @@
-as
+#include<iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Player
+{
+	int id, skills;
+  string name;
+};
+
+int main(){
+  vector<Player> league1;
+
+  league1.push_back({1, 100})
+  league1.push_back({2, 60})
+  league1.push_back({3, 70})
+  league1.push_back({4, 80})
+  league1.push_back({5, 30})
+
+  return 0;
+}
