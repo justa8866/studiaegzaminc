5330d45c35a7debcc1834fa93daa2e8671848718
Filip Wojnicki <filip.wojnicki@op.pl>
2023-05-28 09:31:35 +0200

    zad 1

--- zad1.cpp
+++ zad1.cpp
@@ -13,11 +13,15 @@ struct Player
 int main(){
   vector<Player> league1;
 
-  league1.push_back({1, 100})
-  league1.push_back({2, 60})
-  league1.push_back({3, 70})
-  league1.push_back({4, 80})
-  league1.push_back({5, 30})
+  league1.push_back({1, 100, "John"});
+  league1.push_back({2, 60, "David"});
+  league1.push_back({3, 70, "Paul"});
+  league1.push_back({4, 80, "Daniel"});
+  league1.push_back({5, 30, "Damian"});
+
+  for(const auto& player : league1){
+    cout << "Nazwa: " << player.name << " ID: " << player.id << " skills: " << player.skills << endl;
+  }
 
   return 0;
 }
