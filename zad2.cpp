e5924a9e66a387647a744dc9f21c1c1f4dd5bfcf
Filip Wojnicki <filip.wojnicki@op.pl>
2023-05-28 09:58:04 +0200

    zad 2

--- zad2.cpp
+++ zad2.cpp
@@ -1,7 +1,42 @@
 #include<iostream>
+#include<map>
 
 using namespace std;
 
+template<typename M>
+void print(const M &zawodnicy){
+  for (auto it:zawodnicy){
+    cout << it.first << " " << it.second << endl;
+  }
+}
+
 int main(){
   return 0;
+}
+
+
+int main(){
+  map<int, string> zawodnicy {
+    {1, "Marek"},
+    {2, "Tom"},
+    {3, "Marcin"},
+    {4, "Tadeusz"},
+    {5, "Krzysiek"},
+    {6, "Wojtek"},
+    {7, "Roman"},
+    {8, "Sławek"},
+    {9, "Filip"},
+    {10, "Irek"}
+  };
+
+  print(zawodnicy);
+
+  cout << endl;
+
+  auto zaw = zawodnicy.extract(2);
+  
+  zaw.key() = 20;
+  zawodnicy.insert(move(zaw));
+
+  print(zawodnicy);
 }
