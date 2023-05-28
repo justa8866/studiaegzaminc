37411e07d66365eaad58494985fb0bac7115a2d7
Filip Wojnicki <filip.wojnicki@op.pl>
2023-05-28 09:41:39 +0200

    zad 1

--- zad1.cpp
+++ zad1.cpp
@@ -10,8 +10,15 @@ struct Player
   string name;
 };
 
+void showPlayerVectorContent(vector<Player> wektor) {
+  for(const auto& player : wektor){
+    cout << "Nazwa: " << player.name << " ID: " << player.id << " skills: " << player.skills << endl;
+  }
+}
+
 int main(){
   vector<Player> league1;
+  vector<Player> league2;
 
   league1.push_back({1, 100, "John"});
   league1.push_back({2, 60, "David"});
@@ -19,9 +26,20 @@ int main(){
   league1.push_back({4, 80, "Daniel"});
   league1.push_back({5, 30, "Damian"});
 
-  for(const auto& player : league1){
-    cout << "Nazwa: " << player.name << " ID: " << player.id << " skills: " << player.skills << endl;
+  // showPlayerVectorContent(league1);
+
+  if(league1.size() >= 2) {
+    league1.erase(league1.begin() + 1);
   }
+
+  league1.push_back({6, 60, "Daniel"});
+  league1.push_back({7, 40, "Damian"});
+
+  if(league1.size() >= 2) {
+    league2.insert(league2.begin(), league1.end() - 2, league1.end());
+  }
+
+  showPlayerVectorContent(league2);
 
   return 0;
 }
