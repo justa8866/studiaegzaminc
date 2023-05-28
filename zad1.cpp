45d7571164a942bd58f10e41c4b8334dde3841d9
Filip Wojnicki <filip.wojnicki@op.pl>
2023-05-28 09:50:09 +0200

    zad 1

--- zad1.cpp
+++ zad1.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -16,30 +17,47 @@ void showPlayerVectorContent(vector<Player> wektor) {
   }
 }
 
+bool compareById(const Player& playerA, const Player& playerB) {
+  return playerA.id < playerB.id;
+}
+
+bool compareBySkills(const Player& playerA, const Player& playerB) {
+  return playerA.skills > playerB.skills;
+}
+
 int main(){
   vector<Player> league1;
   vector<Player> league2;
 
   league1.push_back({1, 100, "John"});
   league1.push_back({2, 60, "David"});
-  league1.push_back({3, 70, "Paul"});
-  league1.push_back({4, 80, "Daniel"});
+  league1.push_back({4, 70, "Paul"});
+  league1.push_back({3, 80, "Daniel"});
   league1.push_back({5, 30, "Damian"});
 
-  // showPlayerVectorContent(league1);
+  showPlayerVectorContent(league1);
 
   if(league1.size() >= 2) {
     league1.erase(league1.begin() + 1);
   }
 
-  league1.push_back({6, 60, "Daniel"});
-  league1.push_back({7, 40, "Damian"});
+  league1.push_back({6, 40, "Daniel"});
+  league1.push_back({7, 60, "Damian"});
 
   if(league1.size() >= 2) {
     league2.insert(league2.begin(), league1.end() - 2, league1.end());
   }
 
+  sort(league1.begin(),league1.end(), compareById);
+  showPlayerVectorContent(league1);
+
+  sort(league2.begin(),league2.end(), compareBySkills);
   showPlayerVectorContent(league2);
+
+  league1.clear();
+  league2.clear();
+
+  showPlayerVectorContent(league1);
 
   return 0;
 }
